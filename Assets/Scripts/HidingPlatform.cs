cce0a157a6e64a1d6991867a6ccf438d980bd6fa
CHeol4931 <kmcheol0227@gmail.com>
2025-07-10 02:20:32 +0900

    맴 1스테이지 완성

---
+++ Assets/Scripts/HidingPlatform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HidingPlatform : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("drop", 3f);
+
+        }
+        if (collision.gameObject.CompareTag("trap"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void drop()
+    {
+        gameObject.AddComponent<Rigidbody2D>();
+    }
+}
